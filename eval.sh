14542bc141d4bd9017ee30afcc0df8d5019105b6
root <minhnguyenquocnhat@gmail.com>
2026-01-21 06:33:04 +0000

    upd

--- eval.sh
+++ eval.sh
@@ -1,21 +1,23 @@
 SUBSETS=(
   "ImageNet-1K" "N24News" "HatefulMemes" "VOC2007" "SUN397"
+    # "ImageNet-1K"
 #   "OK-VQA" "A-OKVQA" "DocVQA" "InfographicsVQA" "ChartQA"
 )
 
-MODEL=Path/To/Your/checkpoint
+MODEL=raghavlite/B3_Qwen2_2B
 
 python eval_mmeb.py \
     --model_name $MODEL \
-    --encode_output_path ./MMEB-eval_outputs/Fast_VLM/ \
-    --lora True --lora_r 64 --lora_alpha 64 \
+    --encode_output_path ./MMEB-eval_outputs/B3_Qwen2_2B/ \
+    --lora True --lora_r 8 --lora_alpha 64 \
     --pooling eos \
-    --model_backbone llava_qwen2 \
+    --model_backbone qwen2_vl \
     --normalize True \
     --bf16 \
     --dataset_name TIGER-Lab/MMEB-eval \
     --subset_name "${SUBSETS[@]}" \
     --dataset_split test \
-    --per_device_eval_batch_size 16 \
+    --per_device_eval_batch_size 1 \
     --image_dir eval_images/ \
-    --tgt_prefix_mod
+    --tgt_prefix_mod \
+    --report_to none
