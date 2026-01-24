bcd1ef920f1090f5ebc5f438fcd8466071d8cf62
minhhb9x <minhnguyenquocnhat@gmail.com>
2026-01-24 18:03:20 +0000

    fix

--- train_pooling_teacher.py
+++ train_pooling_teacher.py
@@ -310,11 +310,10 @@ def main():
         drop_last=True,
         pin_memory=False,
     )
-    num_trainable_vision = 0
+    
     for n, p in model_wrapper.model.named_parameters():
         if p.requires_grad:
-            num_trainable_vision += p.numel()
-        print(f'Trainable param: {n}')
+            print(f'Trainable param: {n}')
 
     optimizer = AdamW(
         list(model_wrapper.model.encoder.pool_v.parameters()) +
