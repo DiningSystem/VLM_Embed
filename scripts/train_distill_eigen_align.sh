5a23b3ca087170bd8d74c2724dffd3b0efddbac7
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2026-01-31 02:03:23 +0700

    ER align

--- scripts/train_distill_eigen_align.sh
+++ scripts/train_distill_eigen_align.sh
@@ -39,7 +39,7 @@ torchrun --nproc_per_node=$NUM_GPUS_PER_NODE $TRAIN_SCRIPT \
     --teacher_normalize True \
     --lr_scheduler_type "cosine" \
     --warmup_ratio 0.03 \
-    --kd_weight 0.3 \
+    --kd_weight 1.0 \
     --kd_loss_type "eigen_rank_align_loss" \
     --image_resolution "low" \
     --report_to none 
