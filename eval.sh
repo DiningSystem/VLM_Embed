cfa95dbdc005a08fc89b33a69b96656e3a3f42d5
Ubuntu <gdi-user@atlmartin07.geodigital.com>
2026-02-23 15:36:24 +0000

    eval vqa

--- eval.sh
+++ eval.sh
@@ -1,23 +1,22 @@
 SUBSETS=(
   # "ImageNet-1K" "N24News" "HatefulMemes" "VOC2007" "SUN397"
-    "ImageNet-1K"
-#   "OK-VQA" "A-OKVQA" "DocVQA" "InfographicsVQA" "ChartQA"
+   # "ImageNet-1K"
+   "OK-VQA" "A-OKVQA" "DocVQA" "InfographicsVQA" "ChartQA" "Visual7W"
 )
 
-MODEL=raghavlite/B3_Qwen2_2B
-
+MODEL=./training/eos_er_combined_student/checkpoint-final
 python eval_mmeb.py \
     --model_name $MODEL \
-    --encode_output_path ./MMEB-eval_outputs/B3_Qwen2_2B/ \
+    --encode_output_path ./MMEB-eval_outputs/vqa_combine_er_eos_kl/ \
     --lora True --lora_r 8 --lora_alpha 64 \
     --pooling eos \
-    --model_backbone qwen2_vl \
+    --model_backbone llava_qwen2 \
     --normalize True \
     --bf16 \
     --dataset_name TIGER-Lab/MMEB-eval \
     --subset_name "${SUBSETS[@]}" \
     --dataset_split test \
-    --per_device_eval_batch_size 1 \
+    --per_device_eval_batch_size 16 \
     --image_dir eval_images/ \
     --tgt_prefix_mod \
     --load_pretrained_lora True \
