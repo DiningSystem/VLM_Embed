54e85a399abe22cc815984b1751f5e9ca9de0e95
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2026-02-27 09:29:53 +0700

    fix

--- train_distill_ddp.py
+++ train_distill_ddp.py
@@ -271,7 +271,7 @@ class Trainer:
                 if self.model_args.model_backbone in ["llava_onevision", "llava_two_vision"]:
                     torch.save(student.encoder.model.multi_modal_projector.state_dict(), projector_dir)
                 else:
-                    if hasattr(student.encoder.model, 'mm_projector'):
+                    if hasattr(student.encoder.model.model, 'mm_projector'):
                         torch.save(student.encoder.model.mm_projector.state_dict(), projector_dir)
 
                 student_config = AutoConfig.from_pretrained(self.model_args.model_name) if self.model_args.model_name else None
@@ -301,7 +301,7 @@ class Trainer:
             if self.model_args.model_backbone in ["llava_onevision", "llava_two_vision"]:
                 torch.save(student.encoder.model.multi_modal_projector.state_dict(), projector_dir)
             else:
-                if hasattr(student.encoder.model, 'mm_projector'):
+                if hasattr(student.encoder.model.model, 'mm_projector'):
                     torch.save(student.encoder.model.model.mm_projector.state_dict(), projector_dir)
             student_config = AutoConfig.from_pretrained(self.model_args.model_name) if self.model_args.model_name else None
             tokenizer = AutoTokenizer.from_pretrained(self.model_args.model_name) if self.model_args.model_name else None
