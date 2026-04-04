b36810d59fd08bf74f7ae1e381927e145032b8ba
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-04-04 14:14:03 +0700

    fix_scripts_recursive

--- scripts/train_distill_recursive_vqa.sh
+++ scripts/train_distill_recursive_vqa.sh
@@ -21,10 +21,10 @@ torchrun --standalone \
     --image_dir "vlm2vec_train/MMEB-train" \
     --percent_data 1.0 \
     --output_dir "training/recursive_distill_vqa" \
-    --per_device_train_batch_size 10 \
+    --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-4 \
-    --num_train_epochs 2 \
+    --num_train_epochs 1 \
     --bf16 \
     --save_total_limit 5 \
     --logging_steps 1 \
@@ -33,17 +33,17 @@ torchrun --standalone \
     --weight_decay 0.01 \
     --normalize True \
     --teacher_normalize True \
-    --lr_scheduler_type "cosine" \
+    --lr_scheduler_type "constant" \
     --warmup_ratio 0.03 \
-    --kd_weight 0.3 \
+    --kd_weight 0.6 \
     --kd_loss_type "recursive_distillation_loss" \
-    --recursive_num_steps 6 \
-    --recursive_mean_weight 1.0 \
+    --recursive_num_steps 2 \
+    --recursive_mean_weight 0.1 \
     --recursive_cov_weight 0.1 \
     --recursive_contrastive_weight 1.0 \
     --recursive_attn_weight 1.0 \
     --recursive_enable_kv_cache True \
     --recursive_kv_cache_size 32 \
-    --image_resolution "mid" \
+    --image_resolution "low" \
     --projector_config_path "./config/projector_config.json" \
     --projector_lr 5e-5
