0d1b9d46114973ffb8ce462363801c1d1bf6b766
Ubuntu <gdi-user@atlmartin07.geodigital.com>
2026-04-19 00:35:08 +0000

    train_scripts

--- scripts/train_distill_recursive_vqa.sh
+++ scripts/train_distill_recursive_vqa.sh
@@ -20,8 +20,8 @@ torchrun --standalone \
     --dataset_split "original" \
     --image_dir "vlm2vec_train/MMEB-train" \
     --percent_data 1.0 \
-    --output_dir "training/recursive_distill_vqa" \
-    --per_device_train_batch_size 10 \
+    --output_dir "training/recursive_distill_vqa2" \
+    --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-4 \
     --num_train_epochs 1 \
@@ -35,16 +35,17 @@ torchrun --standalone \
     --teacher_normalize True \
     --lr_scheduler_type "constant" \
     --warmup_ratio 0.03 \
-    --kd_weight 0.6 \
+    --kd_weight 0.7 \
     --kd_loss_type "recursive_distillation_loss" \
     --recursive_num_steps 2 \
     --recursive_backprop_steps 2 \
     --recursive_mean_weight 0.1 \
     --recursive_cov_weight 0.1 \
     --recursive_contrastive_weight 1.0 \
-    --recursive_attn_weight 1.0 \
+    --recursive_attn_weight 0 \
     --recursive_enable_kv_cache True \
     --recursive_kv_cache_size 32 \
     --image_resolution "low" \
     --projector_config_path "./config/projector_config.json" \
-    --projector_lr 5e-5
+    --projector_lr 5e-5 \
+    --report_to wandb
