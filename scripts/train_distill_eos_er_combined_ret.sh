2b6ba577d035a29e9c0384eb711859ff02b06e7a
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-05-03 11:58:24 +0700

    add_ret_scripts3

--- scripts/train_distill_eos_er_combined_ret.sh
+++ scripts/train_distill_eos_er_combined_ret.sh
@@ -51,8 +51,8 @@ torchrun --nproc_per_node=$NUM_GPUS_PER_NODE \
     --eos_projection_space "${EOS_PROJECTION_SPACE}" \
     --eos_kd_weight "${EOS_KD_WEIGHT}" \
     --er_kd_weight "${ER_KD_WEIGHT}" \
-    --image_resolution "low" \
+    --image_resolution "mid" \
     --projector_config_path "./config/projector_config.json" \
-    --projector_lr 5e-5 \
+    --projector_lr 5e-4 \
     --ddp_find_unused_parameters True \
     --report_to None
